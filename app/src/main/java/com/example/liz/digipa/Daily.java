8236b892f3c652d061a19465746ed3dee7d21663
Elisabeth Holtman <holtman.9@buckeyemail.osu.edu>
2014-11-07 11:06:35 -0500

    Added daily java file and xml, and made changes to string file

---
+++ app/src/main/java/com/example/liz/digipa/Daily.java
@@ -0,0 +1,42 @@
+package com.example.liz.digipa;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Daily extends Activity {
+
+    TextView dateHeading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily);
+
+        dateHeading = (TextView)findViewById(R.id.date);
+        dateHeading.setText(savedInstanceState.getString(dateChosen));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.daily, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
