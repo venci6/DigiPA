94dd263431be69612c4593ed678a0f2b71f62faa
Elisabeth Holtman <holtman.9@buckeyemail.osu.edu>
2014-12-01 18:46:01 -0500

    fixed insert statement

--- app/src/main/java/com/example/liz/digipa/Settings.java
+++ app/src/main/java/com/example/liz/digipa/Settings.java
@@ -2,9 +2,13 @@ package com.example.liz.digipa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 /**
  * Created by Charlene on 11/14/2014.
+ * Updated by Liz 11/21/2014.
  */
 public class Settings extends Activity {
 
@@ -12,5 +16,15 @@ public class Settings extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+    }
+
+    //When checkbox is clicked to enable FB sync, set EditText fields to prompt for auth. info
+    public void onGetFBEvents(View v) {
+        if(((CheckBox) v).isChecked()) {
+            EditText getUName = (EditText)findViewById(R.id.FBusername);
+            getUName.setHint(R.string.FBUNameHint);
+            EditText getPword = (EditText)findViewById(R.id.FBpassword);
+            getPword.setHint(R.string.FBPasswordHint);
+        }
     }
 }
